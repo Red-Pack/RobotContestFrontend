34a1c3ab7fa97fe6fc68a03b9d68fb6012ee4e22
Red-Pack <45145832+Red-Pack@users.noreply.github.com>
2024-12-12 10:58:00 +0500

    struct rebuild

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './pages/start-page/start-page.component';
-import { HeaderComponentComponent } from './header-component/header-component.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
 
 export const routes: Routes = [
 
