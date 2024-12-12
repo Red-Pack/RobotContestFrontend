843d615d7e37b44cc60ebec7930e6f919790bbf5
Red-Pack <45145832+Red-Pack@users.noreply.github.com>
2024-12-12 11:49:05 +0500

    request form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './pages/start-page/start-page.component';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { RequestPageComponent } from './pages/request-page/request-page.component';
 
 export const routes: Routes = [
 
     {path: '', component: HeaderComponentComponent, children: [
         {path: '', component: StartPageComponent},
-        {path: 'login', component: LoginPageComponent}
+        {path: 'login', component: LoginPageComponent},
+        {path: 'request', component: RequestPageComponent}
     ]},
 
     
