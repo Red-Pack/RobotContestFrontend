b388974766df9d6326b819f9de7e1c0bb7d90538
Aweb0 <zveriyfanta@gmail.com>
2024-12-12 13:29:02 +0500

    начало окна испытаний

---
+++ src/app/pages/page-testing/page-testing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-testing',
+  imports: [],
+  templateUrl: './page-testing.component.html',
+  styleUrl: './page-testing.component.css'
+})
+export class PageTestingComponent {
+
+}
